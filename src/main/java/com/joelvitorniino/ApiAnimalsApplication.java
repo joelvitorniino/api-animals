fcfda0add0c6e912a092c3de119d4b09e3e36657
Joel Vitor Niino <joelprofessor13@gmail.com>
2022-07-28 19:51:41 -0300

    Project created

---
+++ src/main/java/com/joelvitorniino/ApiAnimalsApplication.java
@@ -0,0 +1,13 @@
+package com.joelvitorniino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAnimalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAnimalsApplication.class, args);
+	}
+
+}
