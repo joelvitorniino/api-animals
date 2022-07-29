f08d188030eee47861e43762a989a6d031cb2df3
Joel Vitor Niino <joelprofessor13@gmail.com>
2022-07-29 20:06:49 -0300

    Get/Random/Insert

--- src/main/java/com/joelvitorniino/resources/MonkeyResource.java
+++ src/main/java/com/joelvitorniino/resources/MonkeyResource.java
@@ -1,16 +1,36 @@
 package com.joelvitorniino.resources;
 
+import com.joelvitorniino.models.Monkey;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.joelvitorniino.services.MonkeyService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/animals/monkey")
 public class MonkeyResource {
+    @Autowired
+    MonkeyService service;
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<String> getMonkey() {
-        return ResponseEntity.ok().body(new MonkeyService().randomPhotoMonkey());
+    public ResponseEntity<List<Monkey>> getMonkey() {
+
+        return ResponseEntity.ok().body(service.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody Monkey objParameter) {
+        Monkey obj = service.insert(objParameter);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @RequestMapping(value = "/random", method = RequestMethod.GET)
+    public ResponseEntity<String> randomMonkey() {
+        return ResponseEntity.ok().body(service.randomPhotoMonkey());
     }
 }
