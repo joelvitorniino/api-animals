0554de7577db468b70f01ebaacf2773e1260d914
Joel Vitor Niino <joelprofessor13@gmail.com>
2022-07-29 19:16:04 -0300

    create monkey

---
+++ src/main/java/com/joelvitorniino/models/Monkey.java
@@ -0,0 +1,52 @@
+package com.joelvitorniino.models;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "links")
+public class Monkey implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String links;
+
+    public Monkey() {
+    }
+
+    public Monkey(Integer id, String links) {
+        this.id = id;
+        this.links = links;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLinks() {
+        return links;
+    }
+
+    public void setLinks(String links) {
+        this.links = links;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Monkey monkey = (Monkey) o;
+        return Objects.equals(id, monkey.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
