b2b7f339cea712b2cee4566f59b2fbe428a83fc7
Joel Vitor Niino <joelprofessor13@gmail.com>
2022-07-29 19:18:50 -0300

    Create Monkey Service

--- src/main/java/com/joelvitorniino/services/MonkeyService.java
+++ src/main/java/com/joelvitorniino/services/MonkeyService.java
@@ -7,11 +7,7 @@ import java.util.Random;
 @Service
 public class MonkeyService {
     public String randomPhotoMonkey() {
-        String[] array = {"teste", "2", "3", "4"};
-        Random r = new Random();
 
-        String word = array[r.nextInt(array.length)];
-
-        return word;
+        return "";
     }
 }
