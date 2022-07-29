dc4233d9a0d62701850406680849e9ce446c759c
Joel Vitor Niino <joelprofessor13@gmail.com>
2022-07-29 19:17:08 -0300

    add test to include data

---
+++ src/main/java/com/joelvitorniino/config/Instantiation.java
@@ -0,0 +1,20 @@
+package com.joelvitorniino.config;
+
+import com.joelvitorniino.models.Monkey;
+import com.joelvitorniino.repository.MonkeyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+    @Autowired
+    private MonkeyRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Monkey monkeyOne = new Monkey(null, "https://");
+
+        repository.save(monkeyOne);
+    }
+}
