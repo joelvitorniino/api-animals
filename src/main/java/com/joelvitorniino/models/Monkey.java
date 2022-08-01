9a2092fcecb0b76c0290ffdb07c621afa6298edd
Joel Vitor Niino <joelprofessor13@gmail.com>
2022-08-01 17:39:24 -0300

    modify table name

--- src/main/java/com/joelvitorniino/models/Monkey.java
+++ src/main/java/com/joelvitorniino/models/Monkey.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
-@Table(name = "links")
+@Table(name = "monkey_links")
 public class Monkey implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
