89fe7a64ea9ffd0acb4df9f6cd8a9ad6de0b3266
Joel Vitor Niino <joelprofessor13@gmail.com>
2022-08-01 17:51:03 -0300

    Create model Fish

---
+++ src/main/java/com/joelvitorniino/models/Fish.java
@@ -0,0 +1,61 @@
+package com.joelvitorniino.models;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "fish_links")
+public class Fish implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String url;
+
+    public Fish() {
+    }
+
+    public Fish(Integer id, String url) {
+        this.id = id;
+        this.url = url;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getURL() {
+        return url;
+    }
+
+    public void setURL(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Fish monkey = (Fish) o;
+        return Objects.equals(id, monkey.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(getURL());
+
+        return sb.toString();
+    }
+}
