140f958ed06ce98ab08cc0c793074cfb228d1d5e
Joel Vitor Niino <joelprofessor13@gmail.com>
2022-08-02 13:37:35 -0300

    Ant Repository

---
+++ src/main/java/com/joelvitorniino/repository/AntRepository.java
@@ -0,0 +1,7 @@
+package com.joelvitorniino.repository;
+
+import com.joelvitorniino.models.Ant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AntRepository extends JpaRepository<Ant, Integer> {
+}
