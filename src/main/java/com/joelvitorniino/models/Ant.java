ff550ff6af4629b47806e030c121b2dc850c5f5a
Joel Vitor Niino <joelprofessor13@gmail.com>
2022-08-02 13:37:02 -0300

    Model ant

---
+++ src/main/java/com/joelvitorniino/models/Ant.java
@@ -0,0 +1,61 @@
+package com.joelvitorniino.models;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "ant_links")
+public class Ant implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String url;
+
+    public Ant() {
+    }
+
+    public Ant(Integer id, String url) {
+        this.id = id;
+        this.url = url;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getURL() {
+        return url;
+    }
+
+    public void setURL(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ant ant = (Ant) o;
+        return Objects.equals(id, ant.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(getURL());
+
+        return sb.toString();
+    }
+}
