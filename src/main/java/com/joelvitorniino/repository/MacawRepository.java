6323c28b47ec422f85b73286406754e7140b3142
Joel Vitor Niino <joelprofessor13@gmail.com>
2022-08-03 16:36:36 -0300

    Macaw repository

---
+++ src/main/java/com/joelvitorniino/repository/MacawRepository.java
@@ -0,0 +1,7 @@
+package com.joelvitorniino.repository;
+
+import com.joelvitorniino.models.Macaw;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MacawRepository extends JpaRepository<Macaw, Integer> {
+}
