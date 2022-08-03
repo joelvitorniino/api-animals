ae4b095fd39248a304920e4c89e4b9a2f9792834
Joel Vitor Niino <joelprofessor13@gmail.com>
2022-08-03 16:38:54 -0300

    Macaw endpoint

---
+++ src/main/java/com/joelvitorniino/resources/MacawResource.java
@@ -0,0 +1,36 @@
+package com.joelvitorniino.resources;
+
+import com.joelvitorniino.models.Macaw;
+import com.joelvitorniino.services.MacawService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/animals/macaw")
+public class MacawResource {
+    @Autowired
+    MacawService service;
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<Macaw>> getFish() {
+
+        return ResponseEntity.ok().body(service.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody Macaw objParameter) {
+        Macaw obj = service.insert(objParameter);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @RequestMapping(value = "/random", method = RequestMethod.GET)
+    public ResponseEntity<String> randomMacaw() {
+        return ResponseEntity.ok().body(service.randomPhotoMacaw());
+    }
+}
