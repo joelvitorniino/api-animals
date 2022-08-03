d3a0a60475dc01b467bbb8cd5d5596595737e09c
Joel Vitor Niino <joelprofessor13@gmail.com>
2022-08-03 16:37:57 -0300

    Macaw service

---
+++ src/main/java/com/joelvitorniino/services/MacawService.java
@@ -0,0 +1,38 @@
+package com.joelvitorniino.services;
+
+import com.joelvitorniino.models.Macaw;
+import com.joelvitorniino.repository.MacawRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Random;
+
+@Service
+public class MacawService {
+    @Autowired
+    private MacawRepository repository;
+
+    public List<Macaw> findAll() {
+        return repository.findAll();
+    }
+
+    public String randomPhotoMacaw() {
+        Random r = new Random();
+        String randomElement = null;
+
+        int numberOfElements = 2;
+
+        for (int i = 0; i < numberOfElements; i++) {
+            int randomIndex = r.nextInt(findAll().size());
+            randomElement = String.valueOf(findAll().get(randomIndex));
+            findAll().remove(randomIndex);
+        }
+
+        return randomElement;
+    }
+
+    public Macaw insert(Macaw obj) {
+        return repository.save(obj);
+    }
+}
