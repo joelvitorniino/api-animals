e3f74a8e7c75292f8a98308b7c01da9cca55b8ac
Joel Vitor Niino <joelprofessor13@gmail.com>
2022-08-06 13:07:00 -0300

    Add DTO Monkey on Endpoint

--- src/main/java/com/joelvitorniino/resources/MonkeyResource.java
+++ src/main/java/com/joelvitorniino/resources/MonkeyResource.java
@@ -1,5 +1,6 @@
 package com.joelvitorniino.resources;
 
+import com.joelvitorniino.dto.MonkeyDTO;
 import com.joelvitorniino.models.Monkey;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/animals/monkey")
@@ -16,14 +18,17 @@ public class MonkeyResource {
     @Autowired
     MonkeyService service;
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<Monkey>> getMonkey() {
+    public ResponseEntity<List<MonkeyDTO>> findAll() {
+        List<Monkey> list = service.findAll();
+        List<MonkeyDTO> listDto = list.stream().map(x -> new MonkeyDTO(x)).collect(Collectors.toList());
 
-        return ResponseEntity.ok().body(service.findAll());
+        return ResponseEntity.ok().body(listDto);
     }
 
     @PostMapping
-    public ResponseEntity<Void> insert(@RequestBody Monkey objParameter) {
-        Monkey obj = service.insert(objParameter);
+    public ResponseEntity<Void> insert(@RequestBody MonkeyDTO objDto) {
+        Monkey obj = service.fromDTO(objDto);
+        obj = service.insert(obj);
 
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
