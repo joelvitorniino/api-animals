558ae62d85fb6d3650e3ea265af905099131d8ae
Joel Vitor Niino <joelprofessor13@gmail.com>
2022-08-06 12:28:26 -0300

    Repository ladybug

---
+++ src/main/java/com/joelvitorniino/repository/LadyBugRepository.java
@@ -0,0 +1,7 @@
+package com.joelvitorniino.repository;
+
+import com.joelvitorniino.models.LadyBug;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LadyBugRepository extends JpaRepository<LadyBug, Integer> {
+}
