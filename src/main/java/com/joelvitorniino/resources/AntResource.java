c0cca452a641c4e5c05adb454dc4c839e784a27a
Joel Vitor Niino <joelprofessor13@gmail.com>
2022-08-06 13:29:55 -0300

    AntDTO on Endpoint

--- src/main/java/com/joelvitorniino/resources/AntResource.java
+++ src/main/java/com/joelvitorniino/resources/AntResource.java
@@ -1,5 +1,6 @@
 package com.joelvitorniino.resources;
 
+import com.joelvitorniino.dto.AntDTO;
 import com.joelvitorniino.models.Ant;
 import com.joelvitorniino.services.AntService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/animals/ant")
@@ -16,14 +18,17 @@ public class AntResource {
     @Autowired
     AntService service;
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<Ant>> getAnt() {
+    public ResponseEntity<List<AntDTO>> findAll() {
+        List<Ant> list = service.findAll();
+        List<AntDTO> listDto = list.stream().map(x -> new AntDTO(x)).collect(Collectors.toList());
 
-        return ResponseEntity.ok().body(service.findAll());
+        return ResponseEntity.ok().body(listDto);
     }
 
     @PostMapping
-    public ResponseEntity<Void> insert(@RequestBody Ant objParameter) {
-        Ant obj = service.insert(objParameter);
+    public ResponseEntity<Void> insert(@RequestBody AntDTO objDto) {
+        Ant obj = service.fromDTO(objDto);
+        obj = service.insert(obj);
 
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
