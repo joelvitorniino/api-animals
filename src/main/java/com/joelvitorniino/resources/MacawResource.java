01c564c4c98867a118086b72489496eacfa7dcf1
Joel Vitor Niino <joelprofessor13@gmail.com>
2022-08-06 13:21:07 -0300

    MacawDTO on endpoint

--- src/main/java/com/joelvitorniino/resources/MacawResource.java
+++ src/main/java/com/joelvitorniino/resources/MacawResource.java
@@ -1,5 +1,6 @@
 package com.joelvitorniino.resources;
 
+import com.joelvitorniino.dto.MacawDTO;
 import com.joelvitorniino.models.Macaw;
 import com.joelvitorniino.services.MacawService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/animals/macaw")
@@ -16,14 +18,17 @@ public class MacawResource {
     @Autowired
     MacawService service;
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<Macaw>> getMacaw() {
+    public ResponseEntity<List<MacawDTO>> findAll() {
+        List<Macaw> list = service.findAll();
+        List<MacawDTO> listDto = list.stream().map(x -> new MacawDTO(x)).collect(Collectors.toList());
 
-        return ResponseEntity.ok().body(service.findAll());
+        return ResponseEntity.ok().body(listDto);
     }
 
     @PostMapping
-    public ResponseEntity<Void> insert(@RequestBody Macaw objParameter) {
-        Macaw obj = service.insert(objParameter);
+    public ResponseEntity<Void> insert(@RequestBody MacawDTO objDto) {
+        Macaw obj = service.fromDTO(objDto);
+        obj = service.insert(obj);
 
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
